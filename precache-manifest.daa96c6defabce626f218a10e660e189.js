38d7c9082c0a07bbec83b96eb8630931c95c504d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-30 09:00:09 +0000

    Deploy STEP-tw/gol-sapanaKale to github.com/STEP-tw/gol-sapanaKale.git:gh-pages

---
+++ precache-manifest.daa96c6defabce626f218a10e660e189.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e18e4e20fa5149cfa07c",
+    "url": "/gol-sapanaKale/static/js/runtime~main.e18e4e20.js"
+  },
+  {
+    "revision": "a1f03a9e1f6212b517c3",
+    "url": "/gol-sapanaKale/static/js/main.a1f03a9e.chunk.js"
+  },
+  {
+    "revision": "3f956256f4a3ae2bb4a1",
+    "url": "/gol-sapanaKale/static/js/1.3f956256.chunk.js"
+  },
+  {
+    "revision": "a1f03a9e1f6212b517c3",
+    "url": "/gol-sapanaKale/static/css/main.c8866335.chunk.css"
+  },
+  {
+    "revision": "7608b82edf0b86d9e697d5ca337a1dda",
+    "url": "/gol-sapanaKale/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/gol-sapanaKale/precache-manifest.daa96c6defabce626f218a10e660e189.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/gol-sapanaKale/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
